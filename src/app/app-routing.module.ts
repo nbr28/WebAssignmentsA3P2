74de6b4c47c5bcd2e884a83024f1c60f4c31a5f2
Natan Ross <nross9@myseneca.ca>
2018-03-10 10:41:54 -0500

    Rounting Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CustomersCreateComponent } from './customers/customers-create/customers-create.component';
+import { Page404Component } from './pages/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component:HomeComponent },
+  {path: 'customers',component:CustomersListComponent },
+  {path: 'customercreate',component:CustomersCreateComponent },
+  {path: '',redirectTo:'\home',pathMatch:'full'},
+  {path: '**',component:Page404Component },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
